77662dd02d5931e12a568e2d73210f032770e399
Kohei Suzuki <eagletmt@gmail.com>
2017-01-03 02:04:07 +0900

    Add getopts

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
 extern crate denv;
 extern crate exec;
+extern crate getopts;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    // TODO: Add `-f` flag
-    let path = ".env";
+    let program = args[0].clone();
+
+    let mut opts = getopts::Options::new();
+    opts.parsing_style(getopts::ParsingStyle::StopAtFirstFree);
+    opts.optopt("f", "filename", "Path to .env file", "FILENAME");
+    opts.optflag("h", "help", "Print help");
+    let matches = opts.parse(&args[1..]).expect("Unable to parse options");
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    if matches.free.is_empty() {
+        print_usage(&program, opts);
+        std::process::exit(1);
+    }
+    let path = matches.opt_str("f").unwrap_or(".env".to_owned());
+
     denv::load(path).expect("Unable to load .env file");
-    let err = exec::execvp(args[1].clone(), args[1..args.len()].into_iter());
+    let err = exec::execvp(matches.free[0].clone(), matches.free);
     panic!("{}", err);
+}
+
+fn print_usage(program: &str, opts: getopts::Options) {
+    let brief = format!("Usage: {} [OPTIONS] COMMAND...", program);
+    print!("{}", opts.usage(&brief));
 }
